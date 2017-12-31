f959d3a19296e7ba343cf485c227babf302bc24b
Erick Dennis <erick.dennis@gmail.com>
2017-12-31 17:54:04 +0100

    WIP

--- test/krakex_test.exs
+++ test/krakex_test.exs
@@ -2,23 +2,57 @@ defmodule KrakexTest do
   use ExUnit.Case
   doctest Krakex
 
-  alias Krakex.Fixture
+  alias Krakex.{Client, Fixture}
 
   defmodule TestClient do
     def get("https://api.kraken.com/0/public/Time", _, _), do: {:ok, Fixture.load!("server_time")}
+    def get("https://api.kraken.com/0/public/Assets", _, _), do: {:ok, Fixture.load!("assets")}
   end
 
-  setup_all do
-    client = %Krakex.Client{http_client: TestClient}
-    {:ok, %{client: client}}
-  end
-
-  describe "server_time/1" do
-    test "foo", %{client: client} do
-      result = client |> Krakex.server_time()
-
-      assert result ==
-               {:ok, %{"rfc1123" => "Sat, 16 Dec 17 10:52:38 +0000", "unixtime" => 1_513_421_558}}
+  describe "public market data" do
+    setup do
+      client = %Client{http_client: TestClient}
+      {:ok, %{client: client}}
     end
+
+    test "server_time/1", %{client: client} do
+      expected = %{"rfc1123" => "Sat, 16 Dec 17 10:52:38 +0000", "unixtime" => 1_513_421_558}
+
+      assert client |> Krakex.server_time() == {:ok, expected}
+    end
+
+    test "assets/1", %{client: client} do
+      expected = %{
+        "XETH" => %{
+          "aclass" => "currency",
+          "altname" => "ETH",
+          "decimals" => 10,
+          "display_decimals" => 5
+        },
+        "XXBT" => %{
+          "aclass" => "currency",
+          "altname" => "XBT",
+          "decimals" => 10,
+          "display_decimals" => 5
+        },
+        "ZEUR" => %{
+          "aclass" => "currency",
+          "altname" => "EUR",
+          "decimals" => 4,
+          "display_decimals" => 2
+        },
+        "ZUSD" => %{
+          "aclass" => "currency",
+          "altname" => "USD",
+          "decimals" => 4,
+          "display_decimals" => 2
+        }
+      }
+
+      assert client |> Krakex.assets() == {:ok, expected}
+    end
+  end
+
+  describe "private user data" do
   end
 end
