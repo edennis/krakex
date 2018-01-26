7a7bf68dd9cb654d9108f956aa8c63a585d23dcc
Erick Dennis <erick.dennis@gmail.com>
2018-01-26 17:51:54 +0100

    Bump version to 0.2.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Krakex.Mixfile do
   def project do
     [
       app: :krakex,
-      version: "0.1.1",
+      version: "0.2.0",
       elixir: "~> 1.4",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
