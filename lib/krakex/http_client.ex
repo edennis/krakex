848a25769a11849aadf74185e0e2e705b4331642
Erick Dennis <erick.dennis@gmail.com>
2019-11-27 15:42:58 +0100

    wip

--- lib/krakex/http_client.ex
+++ lib/krakex/http_client.ex
@@ -3,14 +3,15 @@ defmodule Krakex.HTTPClient do
 
   @type response :: {:ok, term} | {:error, term}
 
-  @client HTTPoison
+  @client :hackney
   @headers [{"accept", "application/json"}]
   @post_headers @headers ++ [{"content-type", "application/x-www-form-urlencoded"}]
 
   @spec get(url :: String.t(), params :: keyword, headers :: list, client :: module) :: response
   def get(url, params, headers, client \\ @client)
       when is_binary(url) and is_list(params) and is_list(headers) do
-    response = client.get(url, headers ++ @headers, params: params)
+    url = build_request_url(url, params)
+    response = client.get(url, headers ++ @headers)
     handle_response(response)
   end
 
@@ -22,19 +23,29 @@ defmodule Krakex.HTTPClient do
     handle_response(response)
   end
 
-  defp handle_response({:ok, response}), do: handle_http_response(response)
-  defp handle_response({:error, %{reason: reason}}), do: {:error, reason}
+  def build_request_url(url, nil), do: url
 
-  defp handle_http_response(%{status_code: status_code, body: body})
-       when status_code in 200..206 do
-    case Jason.decode(body) do
+  def build_request_url(url, params) do
+    cond do
+      Enum.count(params) === 0 -> url
+      URI.parse(url).query -> url <> "&" <> URI.encode_query(params)
+      true -> url <> "?" <> URI.encode_query(params)
+    end
+  end
+
+  defp handle_response({:ok, status_code, _headers, client_ref}) when status_code in 200..206 do
+    {:ok, response_body} = :hackney.body(client_ref)
+
+    case Jason.decode(response_body) do
       {:ok, decoded} ->
         {:ok, decoded}
 
       {:error, %Jason.DecodeError{}} ->
-        {:error, {:invalid, body}}
+        {:error, {:invalid, response_body}}
     end
   end
+
+  defp handle_response({:error, reason}), do: {:error, reason}
 
   defp handle_http_response(%{status_code: status_code, body: body}),
     do: {:error, {status_code, body}}
