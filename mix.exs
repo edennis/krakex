2bc684ad97e86c0cb4eb3959d23a08bd50636e71
Erick Dennis <erick.dennis@gmail.com>
2021-02-26 14:50:10 +0100

    Add credo lib

--- mix.exs
+++ mix.exs
@@ -35,6 +35,7 @@ defmodule Krakex.Mixfile do
       {:jason, "~> 1.1"},
       {:excoveralls, "~> 0.8", only: :test},
       {:mix_test_watch, "~> 0.5", only: :dev, runtime: false},
+      {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 0.5.0", only: [:dev, :test], runtime: false},
       {:ex_doc, "~> 0.16", only: :dev, runtime: false}
     ]
